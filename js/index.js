a303324090a05061a3b1e8f5c3a99933868f60ca
Ignacio Skibski <nanoskibski@gmail.com>
2024-08-13 23:27:19 -0300

    listo el responsive para movil

--- js/index.js
+++ js/index.js
@@ -21,3 +21,14 @@ function resetEffect() {
 document.addEventListener('DOMContentLoaded', (event) => {
     typeEffect();
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    var navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+    var navbarCollapse = document.querySelector(".navbar-collapse");
+
+    navLinks.forEach(function (link) {
+      link.addEventListener("click", function () {
+        navbarCollapse.classList.remove("show");
+      });
+    });
+  });
